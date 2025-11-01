fa87cad6315a4afe56281b5f642949766388b935
Yuyue777 <YZ2205@msmnyc.edu>
2025-10-31 21:23:21 -0400

    g

--- intro.java
+++ intro.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 public class intro {
-    public static void main(String[] args)
-    {
-
+    public static void main() {
+        int[] result = new int[2];
         Scanner scanner = new Scanner(System.in); // 从控制台读取
-        int total = 0;
-        int i;
-        for (i = 0; i <= 3; i++){
+        int[] nums = new int[5];
+
+
+
+        for (int i = 0; i <= 4; i++) {
             int num = scanner.nextInt();
-            total = total + num;
+            nums[i] = num;
         }
 
-        System.out.println("The total is" + ": "+ total);
+        System.out.println("Input target");
+        int target = scanner.nextInt();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            while (j <= nums.length - 1) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    System.out.println(result[0] + ", " + result[1]);
+                }
+             j++;
+            }
+            if (result[0] != 0 && result[1] != 0){
+                break;
+            }
+        }
 
     }
 }
