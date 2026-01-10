2fc55221cb04dd47da6238b526b34789690085fd
Yuyue777 <YZ2205@msmnyc.edu>
2026-01-09 21:04:58 -0500

    ll

--- intro.java
+++ intro.java
@@ -1,35 +1,26 @@
-import java.util.Arrays;
-public class intro {
-    public static void main(String[] args) {
-        int[] arr = {5, 6, 8, 1, 3, 4, 2, 9, 7};
-        partition(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
-    }
-    private static void partition(int[] arr, int left, int right){
-        int pivot = arr[right];
-        int indexS = right - 1;
-        int indexB = left;
-
-        while(indexS > indexB){
-            while(arr[indexS] >= pivot){
-                indexS--;
-            }
-            while(arr[indexB] <= pivot) {
-                indexB++;
-            }
-           swap(arr, indexS, indexB);
+// Definition for a binary tree node.
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+}
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }else if (p == null || q == null){
+            return false;
+        }else if (p.val != q.val){
+            return false;
+        }else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
-        swap(arr, indexS, right);
-        if(indexS - 1 - (left) + 1 > 1){
-            partition(arr, left, indexS - 1);
-        }
-        if(right - (indexS + 1) + 1 > 1){
-            partition(arr, indexS + 1, right);
-        }
-    }
-    private static void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
