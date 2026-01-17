e84d9dc457e01d9b821bda7986b3184d53ac4faf
Yuyue777 <YZ2205@msmnyc.edu>
2026-01-16 19:26:10 -0500

    g

--- intro.java
+++ intro.java
@@ -1,7 +1,18 @@
-public class intro {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, I am Emma on Aug 6 2025");
-        System.out.println("Hello, I am Emma on Aug 6 2025");
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+
     }
 }
