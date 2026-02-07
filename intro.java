2c965d696577a01f6d5a9636954ec7c4914a7589
Yuyue777 <YZ2205@msmnyc.edu>
2026-02-06 20:30:41 -0500

    g

--- intro.java
+++ intro.java
@@ -1,30 +1,20 @@
-// Definition for a binary tree node.
-public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+
 class Solution {
-    public int maxDepth(TreeNode root) {
-    if(root == null){
-        return 0;
-    }
-    }
-    public int getMaxDepth(TreeNode root, int level) {
-       if(root.left == null && root.right == null)
-           return level;
-       if(root.left != null && root.right == null)
-           return getMaxDepth(root.left, level + 1);
-       if(root.left == null && root.right != null)
-            return getMaxDepth(root.right, level + 1);
-        if(root.left != null && root.right != null)
-            return Math.max (getMaxDepth(root.right, level + 1),getMaxDepth(root.left, level + 1));
+    public int rob(int[] nums) {
+        int[] arr = new int[n];
+        arr[0] = nums[0];
+        if(nums[1] < arr[0]) {
+            arr[1] = arr[0];
         }
+        int MaxValue = arr[1];
+        for (int i = 2; i < nums.length; i++) {
+            arr[i] = nums[i] + arr[i-2];
+            if(arr[i] < MaxValue){
+                arr[i] = MaxValue;
+            }else{
+                MaxValue = arr[i];
+            }
+        }
+        return MaxValue;
+    }
 }
