ffe0cd0618a585a9d50ec09f7421bc18c358ede9
Yuyue777 <YZ2205@msmnyc.edu>
2026-02-20 19:29:45 -0500

    g

--- intro.java
+++ intro.java
@@ -1,6 +1,10 @@
 
 class Solution {
-   for(int i = 0; i < nums.length; i++) {
+
+    HashMap<Integer, Integer>Sites=new HashMap<Integer, Integer> ();
+
+        for (int i = 0; i < nums.length; i++) {
+        Integer x = map.get(num[i]);for(int i = 0; i < nums.length; i++) {
        int x = nums[i];
        if()
     }
