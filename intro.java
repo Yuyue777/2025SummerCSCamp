795d0e7b908b3c83c60f10ce83014b37d8c5178f
Yuyue777 <YZ2205@msmnyc.edu>
2026-02-20 19:28:14 -0500

    g

--- intro.java
+++ intro.java
@@ -1,27 +1,7 @@
 
 class Solution {
-    public int lengthOfLongestSubstring(String A){
-        int left = 0;
-        int n = A.length();
-        int right = left;
-        int result = 0;
-        Set<character> dic = new HashSet<character>();
-        while (right < n){
-            char a = A.charAt(right);
-            if(dic.contains(a)) {
-                result = Math.max(right - left, result);
-                while(dic.contains(A.charAt(left)){
-                    dic.remove(A.charAt(left));
-                    left++;
-                }
-                dic.add(a);
-                right++;
-            }else{
-                right++;
-                dic.add(a);
-            }
-
-        }
-        return result;
+   for(int i = 0; i < nums.length; i++) {
+       int x = nums[i];
+       if()
     }
 }
