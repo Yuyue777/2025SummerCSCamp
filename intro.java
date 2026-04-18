73f068c30b600f3343a79bf0806e59d2778e0fd1
Yuyue777 <YZ2205@msmnyc.edu>
2026-04-17 21:13:03 -0400

    g

--- intro.java
+++ intro.java
@@ -1,21 +1,9 @@
 class Solution {
-    public int mySqrt(int x) {
-        if(x<2){
-            return x;
+    public int singleNumber(int[] nums) {
+    int result = 0;
+        for (int num : nums) {
+            result ^= num;
         }
-     int left = 0;
-     int right = x;
-
-     while(right - left > 1){
-         int mid = x/2;
-         if(mid * mid > x){
-             right = mid;
-         }else if(mid * mid < x){
-             left = mid;
-         }else{
-             return mid;
-         }
-     }
-     return (right + left) / 2;
+        return result;
     }
 }
