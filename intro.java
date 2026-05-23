d57ee47d02bdeabba449de9b5e05dd825f3f0902
Yuyue777 <YZ2205@msmnyc.edu>
2026-05-22 21:53:21 -0400

    g

--- intro.java
+++ intro.java
@@ -1,9 +1,23 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-    int result = 0;
-        for (int num : nums) {
-            result ^= num;
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLength = nums.length + 1;
+        for (int i = 0; i < nums.length; i++){
+            int sum = 0;
+            for(int j = i; j < nums.length; j++){
+                sum = sum + nums[j];
+                if(sum >= target){
+                    int currentLength = j - i + 1;
+                    if(currentLength < minLength){
+                        minLength = currentLength;
+                    }
+                    break;
+                }
+            }
         }
-        return result;
+        if(minLength == nums.length + 1){
+            return 0;
+        }else{
+            return minLength;
+        }
     }
 }
